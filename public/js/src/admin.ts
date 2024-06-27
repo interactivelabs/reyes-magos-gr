93edc3e37ff3fcc3491bfbd622260b56804fe041
Luis Gonzalez <luis.gonzalez@airbnb.com>
2024-06-27 15:28:09 -0700

    Separated source and compiled js and css

--- public/js/src/admin.ts
+++
@@ -1,5 +0,0 @@
-import initCodes from "./admin/codes";
-import initOrders from "./admin/orders";
-
-initCodes();
-initOrders();
--- public/js/src/app.ts
+++
@@ -1,7 +0,0 @@
-import initAdminNav from "./admin/admin.nav";
-import initNav from "./app/nav";
-import initMycodes from "./app/myCodes";
-
-initMycodes();
-initAdminNav();
-initNav();
