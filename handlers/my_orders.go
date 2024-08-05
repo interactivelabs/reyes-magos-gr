fdb2929f2a518852803e95e7b8d3e062a4aafb9c
Luis Gonzalez <luis.gonzalez@airbnb.com>
2024-08-05 15:09:30 -0700

    continue with complete order

--- handlers/my_orders.go
+++ handlers/my_orders.go
@@ -2,15 +2,19 @@ package handlers
 
 import (
 	"net/http"
+	"reyes-magos-gr/db/repository"
 	"reyes-magos-gr/lib"
 	"reyes-magos-gr/services"
+	"reyes-magos-gr/views/components"
 	my_orders "reyes-magos-gr/views/volunteer/my_orders"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
 )
 
 type MyOrdersHandler struct {
 	VolunteersService services.VolunteersService
+	Ordersrepository repository.OrdersRepository
 }
 
 func (h MyOrdersHandler) MyOrdersViewHandler(ctx echo.Context) error {
@@ -25,4 +29,30 @@ func (h MyOrdersHandler) MyOrdersViewHandler(ctx echo.Context) error {
 	}
 
 	return lib.Render(ctx, my_orders.MyOrders(orders))
+}
+
+func (h MyOrdersHandler) MyOrdersCompleteHandler(ctx echo.Context) error {
+	profile := lib.GetProfileView(ctx)
+	if profile.Email == "" {
+		return echo.NewHTTPError(http.StatusUnauthorized, "Unauthorized")
+	}
+
+	orderIDStr := ctx.Param("order_id")
+	orderId, err := strconv.ParseInt(orderIDStr, 10, 64)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "Invalid order ID")
+	}
+
+	order, err := h.Ordersrepository.GetOrderByID(orderId)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+	}
+
+	order.Completed = 1
+	err = h.Ordersrepository.UpdateOrder(order)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+	}
+
+	return lib.Render(ctx, components.OrderCard(order))
 }
