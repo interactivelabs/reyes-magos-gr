54359b72b1f9b434fcaba3df0f80afaeca87cd1a
Luis Gonzalez <luis.gonzalez@airbnb.com>
2024-12-20 18:14:18 -0800

    FE validation for code is working

---
+++ assets/js/app/redeem.ts
@@ -0,0 +1,45 @@
+/// <reference types="alpinejs" />
+import type { Alpine as AlpineType } from "alpinejs";
+
+declare global {
+  var Alpine: AlpineType;
+}
+
+function validateCodeInput(): boolean {
+  const codeInput = document.getElementById("code") as HTMLInputElement;
+  const code = codeInput.value.toUpperCase().trim();
+  return code.length > 5;
+}
+
+function beforeRequestHandler(evt: Event) {
+  if (!validateCodeInput()) {
+    evt.preventDefault();
+  }
+}
+
+document.addEventListener("alpine:init", () => {
+  Alpine.data("codeForm", () => ({
+    valid: true,
+
+    init() {
+      this.valid = true;
+    },
+
+    submit(evt: Event) {
+      this.valid = validateCodeInput();
+      evt.preventDefault();
+      evt.stopPropagation();
+    },
+
+    update() {
+      const codeInput = document.getElementById("code") as HTMLInputElement;
+      const code = codeInput.value.toUpperCase().trim();
+      codeInput.value = code;
+      if (code.length > 5) {
+        this.valid = true;
+      }
+    },
+  }));
+});
+
+globalThis.beforeRequestHandler = beforeRequestHandler;
